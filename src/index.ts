492cd208f8ec26d9eff86f9ad04dc1291416b6cf
Budi Syahiddin <me@inve.rs>
2024-09-07 16:16:00 +0800

    feat: automated openapi spec generation (#8)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { Logestic } from "logestic";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(swagger())
+	.use(Logestic.preset("common"))
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`[+] 🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
+
+console.log(
+	`[+] View documentation at "${app.server?.url}swagger" in your browser`,
 );
