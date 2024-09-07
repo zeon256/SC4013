b4a0f863062c322ce4a9acae02baea902f424266
Ms Sainah <me@inve.rs>
2024-09-08 00:57:44 +0800

    chore: refactor code to use inbuilt error handling

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,18 @@ import { dbHealthHandler } from "./routes";
 	const cfg = await readJsonConfig();
 	const pool = new Pool(cfg.dbConfig);
 
-	console.log("[+] Connected to postgresql database");
-
 	const app = new Elysia()
 		.state("pool", pool)
 		.use(swagger())
 		.use(Logestic.preset("common"))
+		.onError(({ code, error, set }) => {
+			if (code === "INTERNAL_SERVER_ERROR") {
+				set.status = 500;
+				return "Internal Server Error";
+			}
+
+			return "Internal Server Error";
+		})
 		.get("/", () => "Hello Elysia")
 		.get("/db-healthz", async ({ store: { pool } }) => dbHealthHandler(pool))
 		.listen(cfg.serverConfig?.port ?? 3000);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,8 @@
 import type { Pool } from "pg";
 
 export async function dbHealthHandler(pool: Pool): Promise<string[] | "dead"> {
-	try {
-		const result = await pool.query<string[]>(
-			"SELECT 'Hello, World!' AS message;",
-		);
-		return result.rows[0];
-	} catch (e) {
-		console.error("database might not be alive", e);
-	}
-
-	return "dead";
+	const result = await pool.query<string[]>(
+		"SELECT 'Hello, World!' AS message;",
+	);
+	return result.rows[0];
 }
