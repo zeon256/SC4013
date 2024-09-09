f7b4f8ed4db9a78ff4491fcfb05de9c09d55c0a8
SohWeeKiat <wksoh97@gmail.com>
2024-09-09 22:22:50 +0800

    chore: refactor routes

--- src/index.ts
+++ src/index.ts
@@ -44,12 +44,8 @@ async function tryConnectDb(pool: Pool, cfg: Readonly<AppConfig>) {
 					return "Internal Server Error";
 			}
 		})
-		.get(
-			"/db-healthz",
-			async ({ pool }) => await routes.dbHealth.fn(pool),
-			routes.dbHealth.schema,
-		)
-		.group("/api/v1", (apiGrp) => apiGrp.use(authRoute).use(productRoute(pool)))
+		.group("/api/v1", (apiGrp) => apiGrp.use(productRoute(pool)))
+		.use(routes(pool))
 		.listen(cfg.serverConfig.port);
 
 	console.log(
