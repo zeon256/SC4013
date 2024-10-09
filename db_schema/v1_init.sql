bd89675ec82ca9669d1e7c52081ad301beee4b63
Budi Syahiddin <me@inve.rs>
2024-10-09 12:46:56 +0800

    chore: refactor auth to a single file in v1 folder (#20)
    * chore: refactor auth to a single file in v1 foldere
    
    * chore: fix type in ElysiaSet
    
    * chore: fix typo in schema
    
    * chore: lowercase for local global vars
    
    * fix: reduce error message for auth routes
    
    * fix: too much error for missing jwt_token

--- db_schema/v1_init.sql
+++ db_schema/v1_init.sql
@@ -1,18 +1,18 @@
 -- Create User table
 CREATE TABLE "User"
 (
-    id                          SERIAL PRIMARY KEY,
-    email                       VARCHAR(100)  NOT NULL,
-    password                    VARCHAR(1000) NOT NULL,
-    salt                        VARCHAR(32)   NOT NULL,
-    last_password_change        TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_login                  TIMESTAMPTZ,
-    failed_login_attempt_count  INT NOT NULL DEFAULT 0,
-    lockout                     BOOLEAN       NOT NULL DEFAULT FALSE,
-    is_admin                    BOOLEAN       NOT NULL DEFAULT FALSE,
-    created_at                  TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at                  TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at                  TIMESTAMPTZ   NULL     DEFAULT NULL
+    id                         SERIAL PRIMARY KEY,
+    email                      VARCHAR(100)  NOT NULL,
+    password                   VARCHAR(1000) NOT NULL,
+    salt                       VARCHAR(32)   NOT NULL,
+    last_password_change       TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_login                 TIMESTAMPTZ,
+    failed_login_attempt_count INT           NOT NULL DEFAULT 0,
+    lockout                    BOOLEAN       NOT NULL DEFAULT FALSE,
+    is_admin                   BOOLEAN       NOT NULL DEFAULT FALSE,
+    created_at                 TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at                 TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at                 TIMESTAMPTZ   NULL     DEFAULT NULL
 );
 
 -- Create Product table
@@ -21,9 +21,9 @@ CREATE TABLE Product
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(100) NOT NULL,
     description VARCHAR(1000),
-    created_by   INT          REFERENCES "User" (ID) ON DELETE SET NULL,
-    updated_by   INT          REFERENCES "User" (ID) ON DELETE SET NULL,
-    created_at   TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at   TIMESTAMPTZ  NULL     DEFAULT NULL
+    created_by  INT          REFERENCES "User" (ID) ON DELETE SET NULL,
+    updated_by  INT          REFERENCES "User" (ID) ON DELETE SET NULL,
+    created_at  TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at  TIMESTAMPTZ  NULL     DEFAULT NULL
 );
