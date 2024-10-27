709a44aa7ec180a2432b9e3313e9ca9ac2f63e2c
Budi Syahiddin <me@inve.rs>
2024-10-27 22:28:25 +0800

    cicd: tighten networking (#36)
    * cicd: tighten networking
    
    * docs: update README

--- iac/main.tf
+++ iac/main.tf
@@ -140,32 +140,65 @@ resource "digitalocean_droplet" "sc4013-stg" {
 resource "digitalocean_firewall" "sc4013-firewall" {
   name = "sc4013-firewall"
   droplet_ids = [digitalocean_droplet.sc4013-stg.id]
+
+  # SSH access
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
+
+  # HTTP access from Cloudflare IPs and our IP
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = [
+      "103.21.244.0/22",
+      "103.22.200.0/22",
+      "103.31.4.0/22",
+      "104.16.0.0/13",
+      "104.24.0.0/14",
+      "108.162.192.0/18",
+      "131.0.72.0/22",
+      "141.101.64.0/18",
+      "162.158.0.0/15",
+      "172.64.0.0/13",
+      "173.245.48.0/20",
+      "188.114.96.0/20",
+      "190.93.240.0/20",
+      "197.234.240.0/22",
+      "198.41.128.0/17",
+      "2400:cb00::/32",
+      "2606:4700::/32",
+      "2803:f800::/32",
+      "2405:b500::/32",
+      "2405:8100::/32",
+      "2a06:98c0::/29",
+      "2c0f:f248::/32",
+      "202.55.71.59", # budi IP address 
+      "42.60.14.4" # wee kiat IP address
+    ]
   }
-  inbound_rule {
-    protocol         = "udp"
-    port_range       = "123"
-    source_addresses = ["0.0.0.0/0"]
-  }
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0"]
-  }
+
+  # Outbound rules
   outbound_rule {
     protocol              = "tcp"
-    port_range           = "1-65535"
-    destination_addresses = ["0.0.0.0/0"]
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
+
 # Output the droplet IP
 output "droplet_ip" {
   value = digitalocean_droplet.sc4013-stg.ipv4_address
